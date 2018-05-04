4a0cf58307fe72bf49799bc8cd81a33576e511e0
Thomas <kvucthan@gmail.com>
2018-05-04 10:34:36 +0200

    Mange ændringer fortaget - se basecamp-opgaver udført af THAN d. 3/5-2018 og d. 4/5-2018.

--- partier.js
+++ partier.js
@@ -61,9 +61,10 @@ function init() {
 
     $(".btn-borre").click(function() {
         if (opgavetype == "fordeling") {
-            UserMsgBox("body", "<h3>Hvilke kriterier placerer vi partierne ud fra?</h3><p>Placeringen af partierne på den fordelingspolitiske og den værdipolitiske akse er siden 1995 foretaget ud fra svarene på nogle spørgsmål, der er udvalgt at politologen Ole Borre. De centrale spørgsmål som partier og personer bliver spurgt om i forhold til fordelingspolitik er: <ol><br/><li>Bør folk i højere grad kunne klare sig selv uden overførsler (for eksempel kontanthjælp eller dagpenge) end tilfældet er i dag? </li><br/><li>Skal indtægter beskattes mere eller mindre end de bliver i dag? </li><br/><li>Er indkomstforskellene og forskellene i levestandard i Danmark for store eller for små? </li><br/><li>Skal virksomhederne i højere grad selv have lov til at ordne deres forretninger, eller er det fint at staten går ind og regulerer dem?</li><br/></ol>")
+            UserMsgBox("body", "<h3>Hvilke kriterier placerer vi partierne ud fra?</h3><p>Placeringen af partierne på den fordelingspolitiske og den værdipolitiske akse er siden 1995 foretaget ud fra svarene på nogle spørgsmål, der er udvalgt af politologen Ole Borre. De centrale spørgsmål som partier og personer bliver spurgt om i forhold til fordelingspolitik er: <ol><br/><li>Bør folk i højere grad kunne klare sig selv uden overførsler (for eksempel kontanthjælp eller dagpenge) end tilfældet er i dag? </li><br/><li>Skal indtægter beskattes mere eller mindre end de bliver i dag? </li><br/><li>Er indkomstforskellene og forskellene i levestandard i Danmark for store eller for små? </li><br/><li>Skal virksomhederne i højere grad selv have lov til at ordne deres forretninger, eller er det fint at staten går ind og regulerer dem?</li><br/></ol>")
         } else if (opgavetype == "værdi") {
-            UserMsgBox("body", "<h3>Hvilke kriterier placerer vi partierne ud fra?</h3><p>De spørgsmål placeringen afgøres efter er: <ol><li>Udgør indvandring alvorlig trussel mod vores nationale egenart?</li><li>Bør den økonomiske vækst sikres gennem en udbygning af industrien, også selvom det kommer i strid med miljøinteresser</li><li>Bør voldsforbrydelser straffes langt hårdere end i dag?</li><li>Bruger det offentlige for mange penge, passende eller for få penge på ulandsbistand?</li></ol>")
+            // UserMsgBox("body", "<h3>Hvilke kriterier placerer vi partierne ud fra?</h3><p>De spørgsmål placeringen afgøres efter er: <ol><li>Udgør indvandring alvorlig trussel mod vores nationale egenart?</li><li>Bør den økonomiske vækst sikres gennem en udbygning af industrien, også selvom det kommer i strid med miljøinteresser</li><li>Bør voldsforbrydelser straffes langt hårdere end i dag?</li><li>Bruger det offentlige for mange penge, passende eller for få penge på ulandsbistand?</li></ol>");                                                                                                                                                                                                                                     // Udkommenteret af THAN d. 3/5-2018. 
+            UserMsgBox("body", "<h3>Hvilke kriterier placerer vi partierne ud fra?</h3><p>Placeringen af partierne på den fordelingspolitiske og den værdipolitiske akse er siden 1995 foretaget ud fra svarene på nogle spørgsmål, der er udvalgt af politologen Ole Borre. De centrale spørgsmål som partier og personer bliver spurgt om i forhold til værdipolitik er: <ol><br/><li>Udgør indvandring alvorlig trussel mod vores nationale egenart?</li><br/><li>Bør den økonomiske vækst sikres gennem en udbygning af industrien, også selvom det kommer i strid med miljøinteresser</li><br/><li>Bør voldsforbrydelser straffes langt hårdere end i dag?</li><br/><li>Bruger det offentlige for mange penge, passende eller for få penge på ulandsbistand?</li><br/></ol>");  // Tilføjet af THAN d. 3/5-2018. FR ønsker denne tekst i stedet for.
         }
     })
 
@@ -82,11 +83,14 @@ function init() {
     } else if (opgavetype == "værdi") {
         microhint($(".draggable"), "Partierne er låst fast på den vandrette akse. Træk  partiets logo til den rigtige position på den lodrette akse.");
     } else if (opgavetype == "fri") {
-        microhint($(".draggable"), "Placer partierne på begge akser, som du synes de passer.");
+        microhint($(".draggable"), "Placer partierne på begge akser, som du synes de passer.", true, '#000');
+        microhint($(".glyphicon-edit"), 'Her kan du ændre opgavebeskrivelsen, fx "Placer partierne ift. Skattepolitik og miljøpolitik."', true, '#000');
+        $('.microhint').addClass('removeHintByClick');
     }
 
     if ($(window).width() < 980) {
-        $(".btn_ghost").html("Info");
+        // $(".btn_ghost").html("Info");                                       // Udkommenteret af THAN d. 3/5-2018
+        $(".btn_ghost").removeClass("btn-sm").addClass("btn-xs xs-fontSize");  // Tilføjet af THAN d. 3/5-2018. TLY ønsker sig mindre btns. 
     }
 
     if ($(window).width() < 1000) {
@@ -99,10 +103,21 @@ function init() {
 
 }
 
+$(document).on('click touchend', ".removeHintByClick", function(event) {  // TLY ønsker sig multiple microhints - her fjernes de to første et efter et ved klik.
+    $(this).fadeOut(400).remove();
+});
+
+function removeDescribingTextInCards() {  // Tilføjet af THAN d. 3/5-2018. Denne kaldes i HTML-filen for opgavetype == "fri". Denne funktion fjerner alt partibeskeivende tekst (f_politik) i JSON data.
+    for (var n in jsonData.partier) {
+        jsonData.partier[n].f_politik = '';
+    }
+}
+
 /*$(window).resize(function() {
     koordinatsystem_size = $(".droppable").width();
     $(".koordinatsystem_size").html("Console: " + runde + "<br/>Koordinatsystem_size: " + koordinatsystem_size);
 });*/
+
 
 /*=============================================
 =            DROPPABLE comment block            =
@@ -140,7 +155,7 @@ $(".droppable").droppable({
 
         //ui.draggable.css('top', 100);
         //ui.draggable.css('left', $(this).position().left);
-        console.log(x_grid + ", INDEXOF:" + jsonData.partier[runde].alt_x_placering.indexOf(x_grid))
+        console.log(x_grid + ", INDEXOF:" + jsonData.partier[runde].alt_x_placering.indexOf(x_grid));
 
         //alert("runde: " + runde);
 
@@ -148,9 +163,11 @@ $(".droppable").droppable({
         =             Opgavetype = Fordeling (droppable funktioner)            =
         ======================================================*/
 
+        console.log('drop - runde: ' + runde + ', x_grid: ' + x_grid + ', typeof(x_grid): ' + typeof(x_grid) +  + ', jsonData.partier[runde].alt_x_placering: ' + jsonData.partier[runde].alt_x_placering + ', typeof(jsonData.partier[runde].alt_x_placering): ' + typeof(jsonData.partier[runde].alt_x_placering));
 
         if (opgavetype == "fordeling") {
-            if (jsonData.partier[runde].alt_x_placering.indexOf(x_grid) > -1) {
+
+            if (jsonData.partier[runde].alt_x_placering.indexOf(x_grid) > -1) { 
 
                 UserMsgBox("body", "<h3>Rigtigt</h3><p>Du har placeret partiet indenfor det rigtige område på aksen. <br/>Partiet bliver flyttet på plads for at sikre, at det er korrekt placeret i forhold til de andre partier.");
                 $(".MsgBox_bgr").click(function() {
@@ -284,15 +301,17 @@ $(".droppable").droppable({
 
                     });
                 });
-            } else {
+            } else {  
                 if (y_grid < jsonData.partier[active_num].y_placering) {
-                    placering = "Du har placeret partiet for langt oppe (til venstre på den værdipolitiske akse)."
+                    // placering = "Du har placeret partiet for langt oppe (til venstre på den værdipolitiske akse).";   // Ukommenteret af THAN d. 3/5-2018 efter ønske af FR.
+                    placering = "Partiet har en mere højreorienteret værdipolitik.";                                     // Tilføjet af THAN d. 3/5-2018 efter ønske af FR.
                 } else {
-                    placering = "Du har placeret partiet for langt nede (til højre på den værdipolitiske akse)."
+                    // placering = "Du har placeret partiet for langt nede (til højre på den værdipolitiske akse).";     // Ukommenteret af THAN d. 3/5-2018 efter ønske af FR.
+                    placering = "Partiet har en mere venstreorienteret værdipolitik.";                                   // Tilføjet af THAN d. 3/5-2018 efter ønske af FR.
                 }
 
 
-                UserMsgBox("body", "<h3>Forkert</h3>" + placering);
+                UserMsgBox("body", "<h3><span class='label label-danger'>Forkert</span></h3>" + placering);
                 $(".MsgBox_bgr").click(function() {
                     $(active).animate({
 
